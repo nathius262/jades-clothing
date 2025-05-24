2d5f66c60ea5705d96b3248838f8d1bff7bc3e5b
Nathaniel <egbodonathaniel@yahoo.com>
2025-05-24 20:04:52 +0100

    create image model in the product module

---
+++ src/modules/product/models/image.cjs
@@ -0,0 +1,31 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Image extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            // One-to-many with products
+            Image.belongsTo(models.Product, {
+                foreignKey: 'productId',
+                as: 'product'  // Alias for related product
+            });
+        }
+    }
+    Image.init({
+        url: DataTypes.STRING,
+        alt_text: DataTypes.STRING,
+        is_primary: DataTypes.BOOLEAN
+    }, {
+        sequelize,
+        modelName: 'Image',
+        tableName: 'images'
+    });
+    return Image;
+};

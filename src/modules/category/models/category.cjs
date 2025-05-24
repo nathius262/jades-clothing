cb35512530175d7700f2f217a810152d3efbc289
Nathaniel <egbodonathaniel@yahoo.com>
2025-05-24 20:46:40 +0100

    update category field in model

--- src/modules/category/models/category.cjs
+++ src/modules/category/models/category.cjs
@@ -1,7 +1,7 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+const generateUniqueSlug = require('../../product/utils/slugHelper.cjs'); // Import the slug helper function
+
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
     /**
@@ -11,13 +11,39 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      // Many-to-Many relationship with Product
+      Category.belongsToMany(models.Product, {
+        through: 'ProductCategory',  // Junction table
+        foreignKey: 'categoryId',
+        as: 'products'               // Alias for related products
+      });
     }
   }
   Category.init({
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    slug: {
+      type: DataTypes.STRING,
+      unique: true,  // Ensures the slug is unique
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Category',
+    tableName:'categories',
   });
+
+  Category.beforeValidate(async (category) => {
+    if (!category.slug) {
+      category.slug = await generateUniqueSlug(category.name, Category);
+    }
+  });
+
+  Category.beforeUpdate(async (category) => {
+    if (category.changed('name')) {
+      category.slug = await generateUniqueSlug(category.name, Category);
+    }
+  });
+  
   return Category;
 };

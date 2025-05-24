3b2f5dec1ffd902c218effa59c62eeec84599412
Nathaniel <egbodonathaniel@yahoo.com>
2025-05-24 20:04:31 +0100

    create product-image migraton

---
+++ src/modules/product/migrations/20250524183042-create-product-image-table.cjs
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Products',  // Refers to the Product table
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      alt_text: {
+        type: Sequelize.STRING
+      },
+      is_primary: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('images');
+  }
+};

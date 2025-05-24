380325d8b1ead4d34846f5ce161292ef6829889c
Nathaniel <egbodonathaniel@yahoo.com>
2025-05-24 20:44:59 +0100

    update category fields in  migration files

--- src/modules/category/migrations/20250524190756-create-category.cjs
+++ src/modules/category/migrations/20250524190756-create-category.cjs
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Categories', {
+    await queryInterface.createTable('categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -11,6 +11,14 @@ module.exports = {
       },
       name: {
         type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      slug: {
+        type: Sequelize.DataTypes.STRING, 
+        unique:true, 
+        allowNull:false
       },
       createdAt: {
         allowNull: false,
@@ -23,6 +31,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Categories');
+    await queryInterface.dropTable('categories');
   }
 };

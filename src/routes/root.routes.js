b4d0ff642fda7effe2b9022cddd4aec41b69a786
Nathaniel <egbodonathaniel@yahoo.com>
2025-05-24 19:18:49 +0100

    create admin dashboard

--- src/routes/root.routes.js
+++ src/routes/root.routes.js
@@ -3,10 +3,14 @@ import {
     index_view, shop_view, detail_view
 } from "../controllers/root.controller.js";
 
+import { dashboard_view } from "../controllers/admin.controller.js";
+
+
 const router = Router();
 
 // Home Route
 router.get('/', index_view);
+router.get('/admin', dashboard_view)
 router.get('/shop', shop_view);
 router.get('/product/detail', detail_view);
 

a33596204c35470a171b8f0d2b6d2c7734fc0441
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-03 14:50:37 +0100

    create public product route

---
+++ src/modules/product/routes/Product.routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import * as controller from '../controllers/Product.controller.js';
+import useModuleViews from '../../../middlewares/moduleViews.js';
+import { withPagination } from '../../../middlewares/paginations.js';
+
+
+const router = express.Router();
+
+router.use(useModuleViews('product'));
+
+// Public view routes
+router.get('/', controller.findAll);
+router.get('/:slug', controller.findBySlug);
+
+export default router;

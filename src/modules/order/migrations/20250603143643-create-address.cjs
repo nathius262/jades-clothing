b46d6318293f4055d22b991ef773aa788f0fa522
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-03 15:47:06 +0100

    create migration for address

---
+++ src/modules/order/migrations/20250603143643-create-address.cjs
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('addresses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      order_id: {
+        type: Sequelize.INTEGER
+      },
+      customer_email: {
+        type: Sequelize.STRING
+      },
+      street_address: {
+        type: Sequelize.TEXT
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('addresses');
+  }
+};

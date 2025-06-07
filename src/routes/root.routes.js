a0cf25b279326a19026706d371d9e42afcbc08fe
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-07 02:33:31 +0100

    add terms and conditions

--- src/routes/root.routes.js
+++ src/routes/root.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { 
-    index_view, shop_view, detail_view
+    index_view, shop_view, detail_view, about_view, contact_view, terms_condition_view, privacy_policy_view
 } from "../controllers/root.controller.js";
 
 import { dashboard_view } from "../controllers/admin.controller.js";
@@ -12,6 +12,10 @@ const router = Router();
 router.get('/', index_view);
 router.get('/admin', dashboard_view)
 router.get('/shop', shop_view);
+router.get('/about', about_view);
+router.get('/contact', contact_view);
+router.get('/terms-conditions', terms_condition_view);
+router.get('/privacy-policy', privacy_policy_view);
 
 router.get('/product/detail', detail_view);
 

cb91d71c6b2645eca29c38f6a4cdc8f3c2eb24ec
Nathaniel <egbodonathaniel@yahoo.com>
2025-06-07 01:17:33 +0100

    update root controller

--- src/controllers/root.controller.js
+++ src/controllers/root.controller.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 const page_logo = process.env.PAGELOGO
 
-const index_view = async (req, res) => {
+export const index_view = async (req, res) => {
     try {
         
 
@@ -29,7 +29,7 @@ const index_view = async (req, res) => {
     }
 };
 
-const shop_view = async (req, res) => {
+export const shop_view = async (req, res) => {
     try {
         const { page, limit, offset } = req.query;
         const products = await productService.findAll({limit, offset});
@@ -50,7 +50,7 @@ const shop_view = async (req, res) => {
     }
 };
 
-const detail_view = async (req, res) => {
+export const detail_view = async (req, res) => {
     try {
         res.render('detail', {
             pageTitle: "detail"
@@ -60,56 +60,3 @@ const detail_view = async (req, res) => {
         
     }
 };
-
-
-const checkout_view = async (req, res) => {
-    try {
-        // Get cart from cookies or initialize empty cart
-        let cart = [];
-
-        try {
-            if (!req.session.cart || !req.session.jades_cart) cart = JSON.parse(req.cookies.jades_cart) || JSON.parse(req.cookies.cart)
-            else if (!req.cookies.jades_cart) cart = req.session.jades_cart || req.session.cart
-            else cart = [];
-        } catch (error) {
-            console.log(error)
-            cart = [];
-        }
-
-        
-
-        // Fetch product details for each item in cart
-        const cartItems = await Promise.all(cart.map(async (item) => {
-            const product = await productService.findById(item.product);
-            return {
-                id: item.product,
-                quantity: item.quantity,
-                price: product.price,
-                name: product.name,
-                images: product.images,
-                description: product.description,
-                itemTotal: item.quantity * product.price
-            };
-        }));
-        
-        // Calculate totals
-        const totalQty = cartItems.reduce((sum, item) => sum + item.quantity, 0);
-        const totalPrice = cartItems.reduce((sum, item) => sum + item.itemTotal, 0);
-
-        res.render('checkout', {
-            pageTitle: "Checkout",
-            cartItems,
-            totalQty,
-            totalPrice,
-        });
-    } catch (err) {
-        console.error('Checkout error:', err);
-        res.status(500).render('./errors/500', { 
-            message: 'Internal Server Error', 
-            error: err.message 
-        });
-    }
-};
-
-
-export {index_view, shop_view, detail_view, checkout_view}

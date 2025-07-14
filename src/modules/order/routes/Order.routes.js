19d6fd659f01f71b815c5e1cf9faf886a1ae1fdd
Nathaniel <egbodonathaniel@yahoo.com>
2025-07-14 11:21:08 +0100

    create order complete page

--- src/modules/order/routes/Order.routes.js
+++ src/modules/order/routes/Order.routes.js
@@ -14,6 +14,7 @@ router.get('/checkout', controller.checkout_view);
 router.get('/api/verify-payment', controller.verify_paystack_transaction_view);
 router.get('/api/track-order', controller.track_order_view);
 router.get('/track-order', controller.track_order_page);
+router.get('/complete', controller.order_complete_view);
 router.post('/create-payment-intent', controller.payment_intent_view);
 router.post('/update-payment-intent', controller.update_payment_intent_view);
 // Stripe webhook needs raw body for signature verification

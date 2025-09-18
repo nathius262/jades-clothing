d1f202b8dc37b08a3d01a39b0217d2d92c56a5cd
Nathaniel Egbodo Odo <nathaniel.egbodo@ignition633.org>
2025-09-18 17:39:20 +0100

    create product size model for easier access between product associate

---
+++ src/modules/product/models/productsize.cjs
@@ -0,0 +1,53 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ProductSize extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of DataTypes lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ProductSize.init({
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'products',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+    size_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'sizes',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    price_override: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true, // null = use product's base price
+      validate: { min: 0 }
+    },
+  }, {
+    sequelize,
+    modelName: 'ProductSize',
+    tableName: 'product_sizes'
+  });
+  return ProductSize;
+};
